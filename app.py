b28eaf16e69a5f9cd380dd4df82877523654439f
Vaibhavcste <68741947+Vaibhavcste@users.noreply.github.com>
2020-08-12 15:40:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from spyre import server
+from alpha_vantage.cryptocurrencies import CryptoCurrencies
+import pandas as pd
+from IPython.display import HTML
+#import matplotlib.pyplot as plt
+
+
+class AmazonBuddy(server.App):
+    title = "Cryptoheaven "
+    inputs = [{ "type":"dropdown",
+                "key":"ticker",
+                "label": "companies",
+                "options": [
+                     {"label": "Bitcoin USD(BTC/USD)", "value": "BTC"},
+                     {"label": "Ethereum USD(ETH/USD)", "value": "ETH"},
+                     {"label": "Ripple USD(XRP/USD)", "value": "XRP"}
+             ],
+                "action_id":"table_id"}]
+
+
+    outputs = [{"type":"table",
+                "id":"table_id",}]
+
+    def getData(self, params):
+        ticker = params['ticker']
+        key = '6CGJ8IWIR2UQ361H'
+        cc = CryptoCurrencies(key,output_format='pandas')
+        df, meta = cc.get_digital_currency_daily(symbol=ticker,market='USD')
+
+        #df= pd.read_csv('physical_currency_list.csv')
+        return df
+
+
+
+app = AmazonBuddy()
+app.launch()
+exit
